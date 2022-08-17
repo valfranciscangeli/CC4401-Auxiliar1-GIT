ff6cff00a28074d2d02002aef312b1a64bffdf17
Valeria Franciscangeli <vvfranciscangeli@gmail.com>
2022-08-17 19:21:30 -0400

    chao

--- todolist/views.py
+++ todolist/views.py
@@ -20,11 +20,11 @@ def index(request): #the index view
 			return redirect("/") #reloading the page
 		
 			#Persona A agrega código AQUI!
-        if "taskDelete" in request.POST: #checking if there is a request to delete a todo ## cambio
-        	checkedlist = request.POST["checkedbox"] #checked todos to be deleted
-			for todo_id in checkedlist:
-				todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
-				todo.delete() #deleting todo
+			if "taskDelete" in request.POST: #checking if there is a request to delete a todo ## cambio
+				checkedlist = request.POST["checkedbox"] #checked todos to be deleted
+				for todo_id in checkedlist:
+					todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
+					todo.delete() #deleting todo
 
 	return render(request, "index.html", {"todos": todos, "categories":categories})
 # comentario 2
